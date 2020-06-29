d85a71ba04f84638a25e57b97bbb8747e1afedc2
samitkumarpatel <samitkumarpatel@gmail.com>
2020-06-30 00:04:35 +0200

    Initial commit

---
+++ src/main/java/crb/apmoller/net/ado/javabuildsample/JavaBuildSampleApplication.java
@@ -0,0 +1,13 @@
+package crb.apmoller.net.ado.javabuildsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBuildSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBuildSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crb/apmoller/net/ado/javabuildsample/JavaBuildSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package crb.apmoller.net.ado.javabuildsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBuildSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
